52cb496c07c90ee9df76ef94bc7df0d34d17c38d
java2019 <java_2019@mail.ru>
2017-06-27 14:03:07 +0300

    test2

--- app/src/main/java/com/samples/web/webkitmenu/WebKitPreferencesActivity.java
+++ app/src/main/java/com/samples/web/webkitmenu/WebKitPreferencesActivity.java
@@ -1,7 +1,16 @@
 package com.samples.web.webkitmenu;
 
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+
 /**
  * Created by use on 27.06.17.
  */
-public class WebKitPreferencesActivity {
+public class WebKitPreferencesActivity  extends PreferenceActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
